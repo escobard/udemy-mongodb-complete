e9b1b09cc4e4ee03a9dd845ec2ca9afdae14cef9
Daniel Escobar <escobard90@gmail.com>
2018-02-18 21:47:04 -0700

    feat: cleaned up user model class, and exported it

--- mongodb-basics-tests/src/user.js
+++ mongodb-basics-tests/src/user.js
@@ -2,6 +2,17 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+// sets the properties of the schema
 const UserSchema = new Schema({
 	name: String
 })
+
+// this creates the mongoose model class- this is set on the mongoose side NOT the mongoDB side
+const User = mongoose.model(
+	// sets the name of the collection within mongoDB - not the mongoose side
+	'user', 
+	// selects the schema for the model
+	UserSchema)
+
+// we usually only export the model class for later use
+module.exports = User;
