d5953317b35e93c028dc61d1916e01a140248c19
Daniel Escobar <escobard90@gmail.com>
2018-02-19 18:44:37 -0700

    chore: updated user test file

--- mongodb-basics-tests/test/create_test.js
+++ mongodb-basics-tests/test/create_test.js
@@ -4,7 +4,10 @@ const assert = require('assert')
 const User = require('../src/user')
 
 describe("Creating user instances", () =>{
-	it("saves a user", () =>{
+
+	// calls done to tell mocha that everything is done AFTER all other operations
+	// have been executed. done() is available for each it and beforeEach module of the testing app
+	it("saves a user", (done) =>{
 
 		// expects an assertion, checking something
 		// assert( 1 + 1 === 2)
@@ -17,5 +20,18 @@ describe("Creating user instances", () =>{
 
 		// saves joe using the .save() method of mongoose
 		joe.save()
+			.then(() =>{
+				// has joe been saved succesfully?
+
+				// if joe has NOT been saved to MongoDB but exsists in mongoose, joe.isNew === true
+				// if joe HAS been saved to MongoDB then joe.isNew === false
+
+				// this assertion ensures that the user schema has been saved to the database
+				// therefore joe.isNew === false
+				assert(!joe.isNew)
+
+				// tells mocha that we are done after the user has been saved
+				done()
+			}).catch(error => console.log('Error creating user: ', error))
 	})
-})
+}) 
