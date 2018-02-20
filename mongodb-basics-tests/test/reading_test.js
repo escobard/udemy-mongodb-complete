b4395a3aa5fb78f931643024853432037d8808fc
Daniel Escobar <escobard90@gmail.com>
2018-02-20 11:53:00 -0700

    chore: asserted tests for finding a user with a matching id

--- mongodb-basics-tests/test/reading_test.js
+++ mongodb-basics-tests/test/reading_test.js
@@ -12,14 +12,18 @@ describe("Reading users out of the database", () => {
 		joe.save().then(() => done())
 	})
 
-	it("finds all users with the name of joe", () => {
+	it("finds all users with the name of joe", done => {
 		// the .find() method matches the given criteria / query, and finds ALL the data matching
 		// the User object comes from our User model, created with mongoose
 		// returns an array
 		User.find({ name: "Joe" })
 			// returns an array with any User instances that match the query
 			.then(users => {
-				console.log(users)
+				// since the .id property below is actually an _id.id property within the mongoDB
+				// it creates some confusion for developers - to avoid this gotcha, we can grab the id of
+				// each record by instead using .id
+				assert(users[0].id === joe.id)
+				done()
 			})
 
 		// the .findOne() method matches the first match that meets the criteria
